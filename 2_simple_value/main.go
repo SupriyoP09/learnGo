6525739949004982897a43e8b1e7822aa3e1dd90
Supriyo <supriyopramanik.2006@gmail.com>
2026-04-18 13:54:51 +0000

    Add initial Go files for Hello World, simple values, and variable examples

---
+++ 2_simple_value/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple value
+	fmt.Println(1)
+	//int
+
+	// string
+	fmt.Println("Hello")
+
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// float
+	fmt.Println(3.14)
+	fmt.Println(7.0/3.0)
+}
---
+++ 3_variables/variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "go"
+
+	// infer
+	// var name = "go"
+	// var isAdult bool= true
+
+	// var age int = 30
+
+	// short hand
+	// name := "go"
+
+	// var name string
+
+	// name = "go"
+
+	// var price float32 = 10.5
+
+	// var price = 10.5
+	price := 10.5
+
+	fmt.Println(price)
+}
